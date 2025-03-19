095162c0dde52fd86440c13f87a5e599d7ef5e83
harunshaban-bmz <harun.shaban@bmz-group.com>
2025-03-19 17:26:59 +0100

    Add project files.

---
+++ DaqTraining/CompositionRoot.cs
@@ -0,0 +1,69 @@
+﻿using BMZ.Daq.Keysight970A;
+using BMZ.Daq;
+using BMZ.Instruments.Ports;
+using BMZ.SCPI;
+using BMZ.SCPI.IP;
+using BMZ.TestSystemBase.Data.TestData;
+using Unity;
+using Unity.Injection;
+using Unity.Lifetime;
+using BMZ.PowerSupply;
+using BMZ.PowerSupply.EA;
+using BMZ.SCPI.UART;
+
+namespace DaqTraining
+{
+    internal class CompositionRoot
+    {
+
+        public IDataProvider TestData => DIContainer.Resolve<IDataProvider>();
+
+        public IUnityContainer DIContainer
+        {
+            get;
+            private set;
+        }
+
+        private static CompositionRoot SingletonInstance;
+
+        private CompositionRoot()
+        {
+            DIContainer = new UnityContainer();
+        }
+
+        public static CompositionRoot Instance
+        {
+            get
+            {
+                if (SingletonInstance == null)
+                {
+                    SingletonInstance = new CompositionRoot();
+                }
+
+                return SingletonInstance;
+            }
+        }
+
+        public void Init()
+        {
+            ConfigureUnityContainer();
+        }
+
+        private void ConfigureUnityContainer()
+        {
+            DIContainer.RegisterType<ISCPIFrame>(
+                "Daq",
+                new ContainerControlledLifetimeManager(),
+                new InjectionFactory(c => { return new SCPIFrameIp(new PortDataIp { Address = "169.254.60.72", Port = 5025 }); }));
+            DIContainer.RegisterType<IDaq, DaqKeysight970A>(new ContainerControlledLifetimeManager(), new InjectionConstructor(new ResolvedParameter<ISCPIFrame>("Daq")));
+
+            //PS();
+            //DIContainer.RegisterType<ISCPIFrame>("EA", new InjectionFactory(c => { return new SCPIFrameUart(c.Resolve<IPortDetection>("Instruments").Ports.Single(x => x.InstrumentType == "EA")); }));
+            DIContainer.RegisterType<ISCPIFrame>(
+                "EA",
+                new ContainerControlledLifetimeManager(),
+                new InjectionFactory(x => { return new SCPIFrameUart(new PortDataUart { ComPort = "COM6", BaudRate = 9600 }); }));
+            DIContainer.RegisterType<IPowerSupply, PowerSupplyEA>(new ContainerControlledLifetimeManager(), new InjectionConstructor(new ResolvedParameter<ISCPIFrame>("EA")));
+        }
+    }
+}
---
+++ DaqTraining/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using BMZ.Daq;
+using BMZ.Daq.Keysight970A;
+using BMZ.PowerSupply;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Unity;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DaqTraining
+{
+    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly IDaq _Daq;
+        private readonly IPowerSupply _PowerSupply;
+        public MainWindow()
+        {
+            InitializeComponent();
+            CompositionRoot.Instance.Init();
+
+            _Daq = CompositionRoot.Instance.DIContainer.Resolve<IDaq>();
+            _PowerSupply = CompositionRoot.Instance.DIContainer.Resolve<IPowerSupply>();
+
+            // Getting Daq reading real time
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(Daq);
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Start();
+        }
+
+        private void PowerSupply(int voltage)
+        {
+            _PowerSupply.General.Connection.Connect();
+            _PowerSupply.Commons.PerformReset();
+            Thread.Sleep(500);
+            _PowerSupply.Voltage.GetVoltage();
+            _PowerSupply.Voltage.SetVoltage(voltage * 1000);
+            StatusTextBlock.Text = $"Voltage initialized to {VoltageTextBox.Text}";
+            Thread.Sleep(500);
+            _PowerSupply.Output.SetOutput(true);
+            StatusTextBlock.Text = $"Voltage set to {VoltageTextBox.Text}";
+            _PowerSupply.General.Connection.Disconnect();
+        }
+
+        private void Daq(object sender, EventArgs e)
+        {
+            _Daq.General.Connection.Connect();
+            _Daq.General.Configure("C:\\Users\\Harun.Shaban\\Downloads\\DaqSettings.xml");
+            DaqReadingTextBox.Text = _Daq.Measurement.Measure("BatteryVoltage").ToString();
+        }
+
+        private void SetButton_Click(object sender, RoutedEventArgs e)
+        {
+            //checking for numeric input
+            bool success = int.TryParse(VoltageTextBox.Text, out int number);
+            if (success)
+            {
+                int setVoltage = int.Parse(VoltageTextBox.Text);
+                // checking for correct input
+                if (setVoltage < 0 || setVoltage > 30)
+                {
+                    StatusTextBlock.Text = "Entered Value must be between 0 and 30";
+                }
+                PowerSupply(setVoltage);
+            }
+            else
+            {
+                StatusTextBlock.Text = "Entered Value must be number";
+            }
+        }
+    }
+}
