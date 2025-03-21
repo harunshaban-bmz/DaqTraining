c46f437ba6dbd1cf98c25a5d30c05afc8dc32604
harunshaban-bmz <harun.shaban@bmz-group.com>
2025-03-21 09:43:23 +0100

    latest for auto charging

--- DaqTraining/MainWindow.xaml.cs
+++ DaqTraining/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using BMZ.Daq;
 using BMZ.Daq.Keysight970A;
 using BMZ.PowerSupply;
+using System;
 using System.Diagnostics;
 using System.Text;
 using System.Windows;
@@ -26,6 +27,9 @@ namespace DaqTraining
     {
         private readonly IDaq _Daq;
         private readonly IPowerSupply _PowerSupply;
+        private bool charged = true;
+        public double measuredVoltage;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -34,24 +38,40 @@ namespace DaqTraining
             _Daq = CompositionRoot.Instance.DIContainer.Resolve<IDaq>();
             _PowerSupply = CompositionRoot.Instance.DIContainer.Resolve<IPowerSupply>();
 
+            //if (charged)
+            //{
+            //    PowerSupply(37, 0);
+            //    StatusTextBlock.Text = $"{measuredVoltage} - Charged";
+            //}
+
+            //else if(!charged)
+            //{
+            //    PowerSupply(37, 5);
+            //}
+
+
             // Getting Daq reading real time
             DispatcherTimer timer = new DispatcherTimer();
             timer.Tick += new EventHandler(Daq);
             timer.Interval = new TimeSpan(0, 0, 1);
             timer.Start();
+
+            DispatcherTimer timer2 = new DispatcherTimer();
+            timer2.Tick += new EventHandler(VoltageCheck);
+            timer2.Interval = new TimeSpan(0, 0, 1);
+            timer2.Start();
         }
 
-        private void PowerSupply(int voltage)
+        private void PowerSupply(int voltage, int current)
         {
             _PowerSupply.General.Connection.Connect();
             _PowerSupply.Commons.PerformReset();
             Thread.Sleep(500);
-            _PowerSupply.Voltage.GetVoltage();
             _PowerSupply.Voltage.SetVoltage(voltage * 1000);
-            StatusTextBlock.Text = $"Voltage initialized to {VoltageTextBox.Text}";
+            _PowerSupply.Current.SetCurrent(current * 1000);
             Thread.Sleep(500);
             _PowerSupply.Output.SetOutput(true);
-            StatusTextBlock.Text = $"Voltage set to {VoltageTextBox.Text}";
+            StatusTextBlock.Text = "Charging";
             _PowerSupply.General.Connection.Disconnect();
         }
 
@@ -59,27 +79,60 @@ namespace DaqTraining
         {
             _Daq.General.Connection.Connect();
             _Daq.General.Configure("C:\\Users\\Harun.Shaban\\Downloads\\DaqSettings.xml");
-            DaqReadingTextBox.Text = _Daq.Measurement.Measure("BatteryVoltage").ToString();
+            measuredVoltage =  Math.Round(_Daq.Measurement.Measure("BatteryVoltage")/1000, 4);
+            DaqReadingTextBox.Text = measuredVoltage.ToString();
+        }
+
+        private void VoltageCheck(object sender, EventArgs e)
+        {
+            Debug.WriteLine($"VoltageCheck: measuredVoltage = {(int)measuredVoltage}");
+            if((int)measuredVoltage == 37)
+            {
+                _PowerSupply.General.Connection.Connect();
+                _PowerSupply.Current.SetCurrent(0);
+                Thread.Sleep(500);
+                _PowerSupply.Output.SetOutput(true);
+                Thread.Sleep(500);
+                StatusTextBlock.Text = "Checking";
+                Thread.Sleep(60000);
+                if((int)measuredVoltage != 36)
+                {
+                    _PowerSupply.General.Connection.Disconnect();
+                    PowerSupply(38, 5);
+                }
+                else
+                {
+                    _PowerSupply.General.Connection.Disconnect();
+                }
+            }
+            //_PowerSupply.Output.SetOutput(false);
         }
 
         private void SetButton_Click(object sender, RoutedEventArgs e)
         {
-            //checking for numeric input
-            bool success = int.TryParse(VoltageTextBox.Text, out int number);
-            if (success)
-            {
-                int setVoltage = int.Parse(VoltageTextBox.Text);
-                // checking for correct input
-                if (setVoltage < 0 || setVoltage > 30)
-                {
-                    StatusTextBlock.Text = "Entered Value must be between 0 and 30";
-                }
-                PowerSupply(setVoltage);
-            }
-            else
-            {
-                StatusTextBlock.Text = "Entered Value must be number";
-            }
+            PowerSupply(38, 5);
+
+            ////checking for numeric input
+            //int number;
+            //bool successVoltage = int.TryParse(VoltageTextBox.Text, out number);
+            //bool successCurrent = int.TryParse(CurrentTextBox.Text, out number);
+
+            //if (successVoltage && successCurrent)
+            //{
+            //    int setVoltage = int.Parse(VoltageTextBox.Text);
+            //    int setCurrent = int.Parse(CurrentTextBox.Text);
+            //    // checking for correct input
+            //    if ((setVoltage < 0 || setVoltage > 40) && (setCurrent < 0 || setCurrent > 5))
+            //    {
+            //        StatusTextBlock.Text = "Voltage should be max 40 and Current max 5";
+            //    }
+            //    PowerSupply(setVoltage, setCurrent);
+            //}
+            //else
+            //{
+            //    StatusTextBlock.Text = "Entered Value must be number";
+            //}
         }
+
     }
 }
